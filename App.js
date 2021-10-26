730c9df3eac8d6483df78044bb9270f4bee53be2
asobima13 <asobima7@gmail.com>
2021-10-26 16:55:53 +0700

    install and implement AsycStorage to prevent OnboardingScreen to show up after first launch

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import * as React from 'react';
-// import { Platform } from 'react-native';
+import { useState, useEffect } from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import OnboardingScreen from './screens/OnboardingScreen';
 import LoginScreen from './screens/LoginScreen';
@@ -9,19 +10,40 @@ import LoginScreen from './screens/LoginScreen';
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false
-          // headerShown: Platform.OS === 'android'
-        }}
-      >
-        <Stack.Screen name="Onboarding" component={OnboardingScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+
+  const [isFirstLaunch, setIsFirstLaunch] = useState(null)
+
+  useEffect(() => {
+    AsyncStorage.getItem('alreadyLaunched').then(value => {
+      if(value === null) {
+        AsyncStorage.setItem('alreadyLaunched', 'true')
+        setIsFirstLaunch(true)
+      } else {
+        setIsFirstLaunch(false)
+      }
+    })
+  },[])
+
+  if (isFirstLaunch === null) {
+    return null;
+  } else if (isFirstLaunch) {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false
+          }}
+        >
+          <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  } else {
+    return (
+      <LoginScreen />
+    )
+  }
 };
 
 export default App
